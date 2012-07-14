ada0e614d25b463f0fb4d9fe2433a37d0190fd41
Valera Trubachev <vetruvet@gmail.com>
2012-07-13 22:21:29 -0400

    Fix bad push?

--- pages/add_ac.php
+++ pages/add_ac.php
@@ -1,8 +1,8 @@
 <?php
 if (isset($_SESSION['user_id'])) {
-    // update data in mysql database
+    // update data in mysql db
     $stmt = $db->stmt_init();
-    $stmt->prepare('INSERT INTO `roms` (`rom`, `romid`, `version`, `buildfingerprint`, `url`, `md5`, `changelog`, `userid`, `device`, `romversionname`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
+    $stmt->prepare('INSERT INTO `roms` (`rom`, `romid`, `version`, `buildfingerprint`, `url`, `md5`, `changelog`, `userid`, `device`, `romversionname`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
     $stmt->bind_param('sssssssiss', $_POST['rom'], $_POST['romid'], $_POST['version'], $_POST['buildfingerprint'], $_POST['url'], $_POST['md5'], $_POST['changelog'], $_SESSION['user_id'], $_POST['device'], $_POST['romversionname']);
 
     // if successfully updated.

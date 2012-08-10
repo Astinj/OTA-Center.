d96ee312fab505e96cc4b0bd61d7c2e791e704f7
Vetruvet <vetruvet@gmail.com>
2012-08-10 12:29:09 -0400

    send gcm registration to any devices registered before the rom was added

--- pages/add_ac.php
+++ pages/add_ac.php
@@ -10,6 +10,44 @@ if (isset($_SESSION['user_id'])) {
         echo 'Added Successfully';
         echo '<br />';
         echo '<a href="?page=list-roms">View result</a>';
+
+        $stmt = $db->stmt_init();
+        $stmt->prepare('SELECT `reg_id` FROM `ota_devices` WHERE `romid` = ? AND `device` = ? UNION SELECT `reg_id` FROM `ota_devices2` WHERE `romid` = ? AND `device` = ?');
+        $stmt->bind_param('ssss', $_POST['romid'], $_POST['device'], $_POST['romid'], $_POST['device']);
+        $stmt->execute();
+        $stmt->bind_result($regid);
+
+        $regids = array();
+        while ($stmt->fetch()) $regids[] = $regid;
+        $stmt->close();
+
+        if (!empty($regids)) {
+            $ch = curl_init('https://android.googleapis.com/gcm/send');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array(
+                    'registration_ids' => $regids,
+                    'collapse_key' => 'ROM Update Available',
+                    'data' => array(
+                            'info_rom' => $_POST['rom'],
+                            'info_version' => $_POST['version'],
+                            'info_changelog' => $_POST['changelog'],
+                            'info_url' => $_POST['url'],
+                            'info_md5' => $_POST['md5']
+                    ),
+                    'delay_while_idle' => true
+            )));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                    'Content-Type: application/json',
+                    'Authorization: key='.$gcmapikey
+            ));
+
+            $result = curl_exec($ch);
+
+            if (curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200) {
+                echo 'NOTE: Sending GCM notification(s) unsuccessful.<br /><br />';
+            }
+        }
     } else {
         echo 'ERROR';
     }
